481deb8d4838140b6a7f2c27dd3688930a93e24b
FelipePereiradeGodoy <godoypereirafelipe@gmail.com>
2021-03-18 19:09:25 -0300

    Insert do componente Input, e paginas cadastrao de doces, Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+
 
 function Login(){
+    
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    
+    
     return(
-        <div>
-            <h1>Tela de Login</h1>
+        <div id="page-login">
+            <main>
+                <form action="">
+                    <fieldset>
+                        <legend>Login</legend>
+                        
+                        <div>
+                            <label htmlFor="userName">User Name</label>
+                            <input id="userName" value={userName} type="text" onChange={(e) => { setUserName(e.target.value) }} />
+                        </div>
+
+                        <div>
+                            <label htmlFor="password">Password</label>
+                            <input id="password" value={password} type="password" onChange={(e) => { setPassword(e.target.value) }} />
+                        </div>
+
+                        
+                        <button className="button-submit" type="submit">Submit</button>
+                        
+                    </fieldset>
+                </form>
+            </main>
         </div>
     );
 }
